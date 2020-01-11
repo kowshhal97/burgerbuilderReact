c906f3b0f3286d1a843ab58dfba89e6dd93402d4
kowshhal97 <54782841+kowshhal97@users.noreply.github.com>
2020-01-11 15:23:14 -0800

    Introduced state in builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,19 @@ import React from 'react'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.css'
 
-const burger=()=> (
+const burger=(props)=> {
+    const newIngredients=Object.keys(props.ingredients).map(igKey=>{
+        return [...Array(props.ingredients[igKey])].map((_,i) =>{
+            return <BurgerIngredient key={igKey+i} type={igKey}/>
+
+        });
+    });
+    return(
     <div className={classes.Burger}>
     <BurgerIngredient type="bread-top"/>
-    <BurgerIngredient type="salad"/>
-    <BurgerIngredient type="cheese"/>
-    <BurgerIngredient type="meat"/>
+    {newIngredients}
     <BurgerIngredient type="bread-bottom"/>
     </div>);
+}
 
 export default burger;
